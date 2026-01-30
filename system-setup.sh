9f225fa87d6a1967ca422e9f0f86c39531ed2a09
civisrom <167646351+civisrom@users.noreply.github.com>
2026-01-30 19:04:38 +0300

    Update system-setup.sh

--- system-setup.sh
+++ system-setup.sh
@@ -1121,7 +1121,7 @@ DEBIAN_PACKAGES=(
 # Note: linux-headers-$(uname -r) will be automatically replaced with current kernel version
 # You can comment out (#) any package to disable its installation
 UBUNTU_PACKAGES=(
-    linux-headers-$(uname -r)
+    #linux-headers-$(uname -r)
     landscape-common
     update-notifier-common
     ubuntu-keyring
